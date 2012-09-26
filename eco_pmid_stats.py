251d773f271e588517943b90065677c19ddd96b2
Iddo Friedberg <idoerg@gmail.com>
2012-09-26 11:22:55 -0400

    modified:   eco_pmid_stats.py

--- eco_pmid_stats.py
+++ eco_pmid_stats.py
@@ -2,7 +2,7 @@
 from collections import defaultdict
 def parse_eco_pmid(infile):
     eco_id_count = defaultdict(int)
-    eco_id_count2 = defaultdict(int)
+    eco_name_count = defaultdict(int)
     eco_id_meaning = defaultdict(str)
     pmid_eco = defaultdict(list)
     for inline in infile:
@@ -15,10 +15,10 @@ def parse_eco_pmid(infile):
             eco_id = eco_id.strip()
             pmid_eco[pmid].append((eco_id, eco_name))
             eco_id_count[eco_id] += 1
-            eco_id_count2[eco_name] += 1
+            eco_name_count[eco_name] += 1
             eco_id_meaning[eco_id] = eco_name
         
-    return eco_id_count, eco_id_count2, eco_id_meaning, pmid_eco
+    return eco_id_count, eco_name_count, eco_id_meaning, pmid_eco
 
 from itertools import combinations
 def amat_pmid_eco(pmid_eco):
@@ -32,13 +32,25 @@ def amat_pmid_eco(pmid_eco):
 
 import networkx as nx
 import matplotlib.pyplot as plt
-def graph_pmid_eco(pmid_eco):
+def graph_pmid_eco(pmid_eco,eco_name_count):
+    eco_name_numbering = {}
+    eco_numbering_name = {}
+    for i,eco_name in enumerate(eco_name_count):
+        eco_name_numbering[eco_name] = i
+        eco_numbering_name[i] = eco_name
+        
+    # Draw a graph (should I weight edges?)
     G = nx.Graph()
     for eco_list in pmid_eco.values():
-        eco_id_list = [i[0] for i in eco_list]
-        for eco_pair in combinations(eco_id_list,2):
-            G.add_edge(*eco_pair) 
+        eco_name_list = [i[1] for i in eco_list]
+        for eco_pair in combinations(eco_name_list,2):
+            eco1 = eco_name_numbering[eco_pair[0]]
+            eco2 = eco_name_numbering[eco_pair[1]]
+            G.add_edge(eco1, eco2) 
+    for i in range(len(eco_numbering_name)):
+        print i, eco_numbering_name[i]
     nx.draw(G)
+    plt.show()
     return G
 
 
